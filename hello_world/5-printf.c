b3fdee445fdb3a13c4d16ba739f187e84f796961
voicedhealer <vivien.bernardot@gmail.com>
2025-02-18 11:14:54 +0100

    hello prinf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *char in argument
+ *printf for print the char 
+ */
+int main()
+{
+    char mon char = "with proper grammar, but the outcome is a piece of art,";
+    printf ("mon char = %c\n", mon char);
+    return 0;
+}	 
