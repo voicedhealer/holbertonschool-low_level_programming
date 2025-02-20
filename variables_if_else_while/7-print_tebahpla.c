06d180e495c83440edbe1d2d31792bcc6c24cb55
voicedhealer <vivien.bernardot@gmail.com>
2025-02-20 17:19:08 +0100

    alphabet reverse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
-*main - affiche l'aphabet en ordre inverse
-*Return : Always 0 (Success)
-*/
+ * main - Affiche l'alphabet en ordre inverse
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char letter;
-	
-	for (letter = 'z'; letter >= 'a'; letter--)
-	{
-		putchar(letter); 
-	}
-	putchar('\n');
+    char letter;
 
-	return (0);
+    for (letter = 'z'; letter >= 'a'; letter--)
+    {
+        putchar(letter);
+    }
+    putchar('\n');
+
+    return (0);
 }
