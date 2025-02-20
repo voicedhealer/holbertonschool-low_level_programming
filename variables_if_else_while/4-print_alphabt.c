7659eb2f2abf5929f59ecb7f008456fb25b2d460
voicedhealer <vivien.bernardot@gmail.com>
2025-02-20 15:12:36 +0100

    print alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche l'alphabet en minuscules puis en majuscules
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
