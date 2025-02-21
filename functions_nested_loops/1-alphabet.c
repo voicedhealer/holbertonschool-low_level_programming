4ba37d92fdd838f413ef8b0e4b5cf56ca31f89ab
voicedhealer <vivien.bernardot@gmail.com>
2025-02-21 16:01:00 +0100

    main

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
-#include <main.h>
+#include "main.h"
 
-int _putchar(char c);
 /**
-* print_alphabet - prints the alphabet in lowercase, followed by a new line.
-*/
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ */
 void print_alphabet(void)
 {
 	char letter;
