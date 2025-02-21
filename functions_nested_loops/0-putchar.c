ee7504a25895a259a1cf9e482df7ddf578433596
voicedhealer <vivien.bernardot@gmail.com>
2025-02-21 11:38:55 +0100

    fonc write

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
-* j'utilise printf pour putchar
-* et un retour à la ligne
-*/
+ * main - Affiche _putchar suivi d'une nouvelle ligne
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar\n";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		write(1, &str[i], 1);
+	}
 
 	return (0);
 }
