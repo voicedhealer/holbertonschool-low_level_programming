1b75507c753cac17233aafd7dacdd62053c16a8a
voicedhealer <vivien.bernardot@gmail.com>
2025-02-21 16:09:18 +0100

    alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+}
