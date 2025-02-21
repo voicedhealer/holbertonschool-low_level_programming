b3b8dd19e0a59c63c43a5884719316fe4f25ec87
voicedhealer <vivien.bernardot@gmail.com>
2025-02-21 16:21:01 +0100

    ex6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - Calcule la valeur absolue d'un entier
+ * @n: L'entier dont on veut calculer la valeur absolue
+ *
+ * Return: La valeur absolue de n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
