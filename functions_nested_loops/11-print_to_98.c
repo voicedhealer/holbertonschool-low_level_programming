09b313583d8af4a3a65436a76ff0fa6bc20425a7
voicedhealer <vivien.bernardot@gmail.com>
2025-02-21 16:40:58 +0100

    og

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Imprime tous les nombres naturels de n à 98
+ * @n: Le nombre de départ
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
+}
