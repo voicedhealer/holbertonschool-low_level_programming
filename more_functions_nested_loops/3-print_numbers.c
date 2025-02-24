ab0cae1de2832369d54122233aa95f08a9dfdb93
voicedhealer <vivien.bernardot@gmail.com>
2025-02-24 15:07:44 +0100

    print numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,25 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-*
-*
-*
-*/
+ * print_numbers - Imprime les chiffres de 0 à 9 suivis d'un saut de ligne
+ */
 void print_numbers(void)
-
 {
-	char number;
+    char number;
 
-	for (number = '0'; number <= '9';number++)
-	{
-		_putchar(number);
-	}
-	_putchar('\n');
-}
-
-int main(void)
-{	
-	print_numbers();
-	return(0);
+    for (number = '0'; number <= '9'; number++)
+    {
+        _putchar(number);
+    }
+    _putchar('\n');
 }
