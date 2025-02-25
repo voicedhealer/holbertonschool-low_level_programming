f1d0a50c9bc6e50d5692d6ddc981ea7fa5eac11c
voicedhealer <vivien.bernardot@gmail.com>
2025-02-25 09:37:21 +0100

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_line - dessine une ligne droite dans le terminal
+*@n: nombre de trois que le caractérer _ doit être imprimé
+*
+*/
+void print_line(int n);
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; < n n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	}
+}
+	 
