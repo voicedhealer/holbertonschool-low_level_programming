02232920431c7a3e3a951775d43bd7d186b2aca0
voicedhealer <vivien.bernardot@gmail.com>
2025-02-25 15:03:47 +0100

    fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - imprime les nombres de 1 à 100 avec les règles Fizz-Buzz
- *
  * Return: Toujours 0 (Succès)
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", i);
-
-        if (i < 100)
-            printf(" ");
-    }
-    printf("\n");
-
-    return (0);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
