97257b40e7d2604ab5e8bc7158cedbce84c248ba
voicedhealer <vivien.bernardot@gmail.com>
2025-02-25 13:43:11 +0100

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,26 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-*imprime un carré suivie d'une ligne
-*taille du carré
-*
-/*
-void print_square(int size);
+ * print_square - imprime un carré suivi d'une nouvelle ligne
+ * @size: taille du carré
+ */
+void print_square(int size)
 {
 	int i, j;
 
-	if (size <= 0) 
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
-	{	
+	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-			putchar('#');
+				_putchar('#');
 			}
-			
-		putchar('\n');
-			
-		}	
+			_putchar('\n');
+		}
 	}
-}
-	
+}	
