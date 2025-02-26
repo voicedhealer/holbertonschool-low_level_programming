b5169933411391ac1fd04ef7d5390cde766c6936
voicedhealer <vivien.bernardot@gmail.com>
2025-02-26 15:34:40 +0100

    swaps deux valeurs

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 /**
-*creation d'une variable temporaire pour permuter les valeurs
-*
-*
+* swap_int - Swaps the values of two integers
+* @a: Pointer to the first integer
+* @b: Pointer to the second integer
 */
 void swap_int(int *a, int *b)
 {
