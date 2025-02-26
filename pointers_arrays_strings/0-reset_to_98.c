7f2b7d4bc0741ec250630a8a384bae575fad4160
voicedhealer <vivien.bernardot@gmail.com>
2025-02-26 15:00:57 +0100

    pointeur reset

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 /**
- * reset_to_98 - Resets the value pointed to by an integer pointer to 98
- * @n: A pointer to an integer to be updated
- *
- * This function takes a pointer to an integer as a parameter and updates
- * the value it points to, setting it to 98.
- */
+* reset_to_98 - Resets the value pointed to by an integer pointer to 98
+* @n: A pointer to an integer to be updated
+*
+* This function takes a pointer to an integer as a parameter and updates
+* the value it points to, setting it to 98.
+*/
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
