f038965f25d40feb723abcf7226ea6dc0d9185b6
voicedhealer <vivien.bernardot@gmail.com>
2025-02-27 14:33:25 +0100

    8 array entier

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+/**
+*utilisation de 2 int a et n 
+*un espace après la virgule
+*respect l'ordre demander
+*
+/*
+#include <stdio.h>
+
+int main(){
+   int a[5] = {-1024, -198, 98, 298, 402};  
+   int n;
+
+   for (n = 0; n <= 4; n++){
+      printf("array [%d]: %d\n", n, a[n]);
+   }
+   return 0;
+}
