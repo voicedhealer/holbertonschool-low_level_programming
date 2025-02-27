eddc91e040a493e280e2d4a35d3165ed7ce49a4a
voicedhealer <vivien.bernardot@gmail.com>
2025-02-27 13:58:02 +0100

    7 puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*
+*
+*
+*
+*/
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+
+	while (str[length] != '\0')
+		length++;
+
+	start = (length % 2 == 0) ? length / 2 : (length + 1) / 2;
+
+	while (str[start] != '\0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
+}
