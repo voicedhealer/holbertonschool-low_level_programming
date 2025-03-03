829cf4791ab927989237b9968fcc178629d91e8d
voicedhealer <vivien.bernardot@gmail.com>
2025-03-03 16:25:10 +0100

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Localise un caractère dans une chaîne
+ * @s: La chaîne à parcourir
+ * @c: Le caractère à trouver
+ *
+ * Return: Un pointeur vers la première occurrence de c dans s,
+ * ou NULL si le caractère n'est pas trouvé
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
