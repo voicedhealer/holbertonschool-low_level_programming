5b321280c0c30ece4ddf546713677b0b9ac78c4f
voicedhealer <vivien.bernardot@gmail.com>
2025-03-04 16:42:16 +0100

    strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,30 +1,28 @@
 #include <stddef.h>
 
 /**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix
- * @a: Pointer to the start of the matrix (treated as a 1D array)
- * @size: Size of the square matrix (number of rows/columns)
+ * _strpbrk - Locates the first occurrence in the string s of any of the bytes
+ *            in the string accept
+ * @s: The string to search in
+ * @accept: The string containing the characters to match
  *
- * Description: This function calculates and prints the sum of the two
- * diagonals of a square matrix. The matrix is represented as a 1D array,
- * and the diagonals are accessed using pointer arithmetic.
- *
- * Return: void
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                return &s[i];
-            }
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
