816eee3ba8e20b51895f3bb8bbc3143ef769e9d5
voicedhealer <vivien.bernardot@gmail.com>
2025-03-06 16:58:14 +0100

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * j'utilise argc et argv
+ *la condition compare argc 
+ *printf pour afficher le nom
+*/
+
+int main(int argc, char *argv[])
+{
+        if(argc == 1)
+        printf("%s\n", argv[])
+        return(0)
+}
