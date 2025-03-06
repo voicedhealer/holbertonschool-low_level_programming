111f1b6599507b7de58d5601dc6f119055a3f0e1
voicedhealer <vivien.bernardot@gmail.com>
2025-03-06 17:23:05 +0100

    mynameis

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,11 +7,9 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-
-	(void)argc;
 
 	return(0);
 }
