988d5bae0d15875c571a5419e2198c023e6e6c4c
voicedhealer <vivien.bernardot@gmail.com>
2025-03-07 15:47:28 +0100

    strlen recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _strlen_recursion - Returns the length of a string
  * @s: The string to measure
@@ -10,5 +12,5 @@ int _strlen_recursion(char *s)
 	{
 		return 0;
 	}
-	return 1 + _strlen_recursion(s + 1);
+		return 1 + _strlen_recursion(s + 1);
 }
