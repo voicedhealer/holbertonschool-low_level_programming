8a23b3373aeed40a40b543348d9594c7a7255dc0
voicedhealer <vivien.bernardot@gmail.com>
2025-03-11 10:23:21 +0100

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,33 +1,41 @@
 #include <stdlib.h>
 #include <string.h>
-
-/**
- * str_concat - Concatenates two strings into a newly allocated memory block
- * @s1: First string (or NULL)
- * @s2: Second string (or NULL)
- *
- * Return: Pointer to the concatenated string, or NULL on failure
- */
 char *str_concat(char *s1, char *s2)
 {
-		if (s1 == NULL)
-			s1 = "";
-		if (s2 == NULL)
-			s2 = "";
+	int x, c, d;
+	int z;
+	char *a;
 
-		size_t len1, len2;
-		char *result;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (x = 0; s1[x] != '\0'; x++)
+	{
 
-		len1 = strlen(s1);
-		len2 = strlen(s2);
+	}
+	for (z = 0; s2[z] != '\0'; z++)
+	{
 
-		result = malloc(len1 + len2 + 1);
+	}
+	a = malloc((x + z + 1) * sizeof(char));
 
-		if (result == NULL)
-			return (NULL);  /* Allocation failed */
-
-		memcpy(result, s1, len1);               /* Copy first part */
-		memcpy(result + len1, s2, len2 + 1);    /* Copy second part including '\0' */
-
-		return (result);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; c < x; c++)
+	{
+		a[c] = s1[c];
+	}
+	for (d = 0; d < z; d++, c++)
+	{
+		a[c] = s2[d];
+	}
+	a[c] = '\0';
+	return (a);
 }
