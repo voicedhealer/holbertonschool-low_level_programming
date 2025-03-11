5f3ddfd628dde3092272a943cf1fd698111652cc
voicedhealer <vivien.bernardot@gmail.com>
2025-03-11 16:00:00 +0100

    amélioration de la fonction array_range pour une meilleure lisibilité et gestion des erreurs

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
+
 /**
  * array_range - Crée un tableau d'entiers de min à max (inclus)
  * @min: Valeur minimale (incluse)
  * @max: Valeur maximale (incluse)
- * 
- * Return: Pointeur vers le tableau, NULL en cas d'échec
+ *
+ * Return: Pointeur vers le tableau alloué, NULL si erreur
  */
-
-int *array_range(int min, int max) 
+int *array_range(int min, int max)
 {
-    int *arr;
-    int i, len;
+	int *arr;
+	int i, len;
 
-    if (min > max)
-        return (NULL);
-    {
-        len = max - min + 1;
-        arr = malloc(len * sizeof(int));
-        if (arr == NULL)
-            return (NULL);
+	if (min > max)
+		return (NULL);
 
-        for (i = 0; i < len; i++)
-            arr[i] = min + i;
+	len = max - min + 1;  /* Nombre d'éléments */
+	arr = malloc(len * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 
-        return (arr);
-    }
+	/* Remplir le tableau avec min à max */
+	for (i = 0; i < len; i++)
+		arr[i] = min + i;
+
+	return (arr);
 }
+
