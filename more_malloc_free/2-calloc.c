27b0cc621c0279d6946fba27a54ded6264da20c2
voicedhealer <vivien.bernardot@gmail.com>
2025-03-11 16:22:21 +0100

    amélioration de la fonction _calloc : ajout de la vérification des débordements et refactorisation pour une meilleure lisibilité

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,36 +1,38 @@
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * _calloc - Alloue de la mémoire et l'initialise à zéro
  * @nmemb: Nombre d'éléments
  * @size: Taille en octets de chaque élément
- *  
  * Return: Pointeur vers la mémoire allouée, NULL en cas d'échec
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
-	unsigned int i;
-	char *casted_ptr;
+    void *ptr;
+    unsigned int i, total_size;
+    char *casted_ptr;
 
-	/* Gestion des cas speciaux */
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+    /* Gestion des cas particuliers */
+    if (nmemb == 0 || size == 0)
+        return (NULL);
 
-	/* Vérification débordement (comme calloc standard) */
-	if (size && nmemb > (unsigned int)-1 / size)
-		return (NULL);
+    /* Vérification débordement mémoire */
+    if (size && nmemb > UINT_MAX / size)
+        return (NULL);
 
-	/* Allocation mémoire */
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
-		return (NULL);
+    total_size = nmemb * size;
 
-	/* Initialisation à zéro */
-	casted_ptr = ptr;
-	for (i = 0; i < nmemb * size; i++)
-		casted_ptr[i] = 0;
+    /* Allocation mémoire */
+    ptr = malloc(total_size);
+    if (ptr == NULL)
+        return (NULL);
 
-	return (ptr);
+    /* Initialisation à zéro */
+    casted_ptr = ptr;
+    for (i = 0; i < total_size; i++)
+        casted_ptr[i] = 0;
+
+    return (ptr);
 }
 
