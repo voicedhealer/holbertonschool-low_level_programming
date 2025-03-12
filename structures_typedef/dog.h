07728292a681a52d72002e7954cab1b074f144a2
voicedhealer <vivien.bernardot@gmail.com>
2025-03-12 13:32:08 +0100

    ajout de la structure dog avec les attributs name, age et owner

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strtuct dog - dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * 
+ */
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
