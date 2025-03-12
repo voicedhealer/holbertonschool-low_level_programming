1e4ab19a76436f2fa2ab24e0f5a3e17fec80d23d
voicedhealer <vivien.bernardot@gmail.com>
2025-03-12 13:47:06 +0100

    suppression d'une ligne vide dans la structure dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,13 @@
 /**
- * strtuct dog - dog structure
+ * description: dog structure
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  * 
- * Description: dog structure
  */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
-
+/*struct dog - dog structure */
