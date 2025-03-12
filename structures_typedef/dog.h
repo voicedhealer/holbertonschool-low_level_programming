d58c63c9cde991149db1e73d8793af6c0726d6a3
voicedhealer <vivien.bernardot@gmail.com>
2025-03-12 13:37:30 +0100

    ajout de l'attribut owner à la structure dog et amélioration de la documentation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * strtuct dog - dog structure
  * @name: name of the dog
  * @age: age of the dog
+ * @owner: owner of the dog
  * 
+ * Description: dog structure
  */
-
 struct dog {
     char *name;
     float age;
