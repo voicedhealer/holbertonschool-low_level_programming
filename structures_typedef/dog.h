fb8292bf43cec3c29cf6613931f54ca8a645af14
voicedhealer <vivien.bernardot@gmail.com>
2025-03-14 11:44:48 +0100

    modification de la structure dog dans dog.h pour améliorer la documentation et ajouter un typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,22 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG_H_
+#define DOG_H_
 
 /**
- * struct dog - Represents a dog with basic information.
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - Base Struct of the code
+ * @name: Pointer to string for determinate the name of the dog
+ * @age: Variable to float for determinate the age of the dog
+ * @owner: Pointer to string for determinate the owner of the dog
+ * Return: 0 Success
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+	typedef struct dog my_dog;
 
-#endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /* DOG_H_ */
