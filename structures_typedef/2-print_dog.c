dbcd278c61af2036de7483e705610b4c19ba5222
voicedhealer <vivien.bernardot@gmail.com>
2025-03-14 14:04:08 +0100

    amélioration de la fonction print_dog pour gérer les pointeurs nuls et réorganiser les inclusions

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,18 +1,36 @@
-#include <stdio.h>
+#include "main.h"
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_dog - prints a struct dog
- * @d: pointer to struct dog
- *
+ * print_dog - print a struct dog
+ * @d: The pointer to the dog
  * Return: void
  */
+
 void print_dog(struct dog *d)
 {
-    if (d == NULL)   
-    return;
+	if (d == NULL)
+	{
+		return;
+	}
 
-    printf("Name: %s\n", d->name ? d->name : "(nil)");
-    printf("Age: %f\n", d->age);
-    printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
