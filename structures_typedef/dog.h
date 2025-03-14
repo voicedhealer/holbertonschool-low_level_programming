f5be0e5c12789ffc5117401663d6638726febcee
voicedhealer <vivien.bernardot@gmail.com>
2025-03-14 11:43:22 +0100

    suppression d'une ligne vide dans le fichier dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
