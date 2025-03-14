a10ce9945e10c5e78f00d0d85ded22a1d187fd7a
voicedhealer <vivien.bernardot@gmail.com>
2025-03-14 14:12:23 +0100

    suppression de lignes vides inutiles dans le fichier dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,14 +8,12 @@
  * @owner: The owner of the dog
  * @age: The age of the dog
  */
-
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
