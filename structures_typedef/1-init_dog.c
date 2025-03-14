cd98c3df837213cc414ab97e02aeeed100f99a38
voicedhealer <vivien.bernardot@gmail.com>
2025-03-14 09:38:20 +0100

    correction de la fonction init_dog pour utiliser un pointeur struct init_dog et retourner un pointeur sur la structure

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,19 +3,22 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog
+ * @a: pointer to struct dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  *
- * Return: void
+ * Return: init_dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
+    struct init_dog *dog;
 
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+init_dog = malloc(sizeof(struct init_dog));
+if (init_dog == NULL)
+    return (NULL);
+dog->name = name;
+dog->age = age;
+dog->owner = owner;
+return init_dog;
 }
