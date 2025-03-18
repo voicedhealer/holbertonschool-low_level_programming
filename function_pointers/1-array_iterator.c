8cdd7d5cb50ca9921cdcd8a151775d95ea122452
voicedhealer <vivien.bernardot@gmail.com>
2025-03-18 16:28:25 +0100

    correction de la fonction array_iterator pour gérer correctement les pointeurs et les actions sur le tableau

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,24 @@
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - Executes a function on each element of an array
+ * @array: The array to iterate over
+ * @size: The size of the array
+ * @action: A pointer to the function to be executed on each element
+ *
+ * Description: This function applies the given action to each element
+ * of the array if both array and action are not NULL.
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-   int *array[0], *size(0);
-   for (array = 0; array < count; array++)
-   {
-        if (action == NULL && size == NULL)
-   }
-        action(array);
-    
-   
-     
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
