e61812a77d5548eb2623bb3a36b515d709544461
voicedhealer <vivien.bernardot@gmail.com>
2025-03-18 10:20:30 +0100

    ajout de la fonction print_name et de la déclaration de _putchar dans les fichiers d'en-tête

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
