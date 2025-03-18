97c02735d38ecf28df641c821b19e04f6f92e69d
voicedhealer <vivien.bernardot@gmail.com>
2025-03-18 13:19:27 +0100

    ajout des fichiers pour la gestion des pointeurs de fonction et implémentation de la fonction array_iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+   int *array[0], *size(0);
+   for (array = 0; array < count; array++)
+   {
+        if (action == NULL && size == NULL)
+   }
+        action(array);
+    
+   
+     
+}
