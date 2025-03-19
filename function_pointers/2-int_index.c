be877e2e45259c970a1682b3c13d74575e06fd71
voicedhealer <vivien.bernardot@gmail.com>
2025-03-19 10:47:28 +0100

    correction de l'inclusion du fichier d'en-tête dans int_index et amélioration de la gestion des cas d'entrées invalides

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include "function_pointers"
+#include "function_pointers.h"
 #include <stdio.h>
 
 /**
@@ -13,23 +13,22 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-    if (size <= 0)
+	if (size <= 0)
 		return (-1);
 
-    if (array == NULL)
+	if (array == NULL)
 		return (-1);
 
-    if (cmp == NULL)
+	if (cmp == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
-        
 		if (cmp(array[i]) != 0)
-		{	
-        return (i);
-        }
+		{
+			return (i);
+		}
+	}
 
-    }
 	return (-1);
 }
