5564797df7e767b8f561ea52a1734bd46ab5bd1b
voicedhealer <vivien.bernardot@gmail.com>
2025-03-19 11:54:35 +0100

    simplification de la documentation de la fonction print_name pour une meilleure clarté

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,9 @@
 #include <stdlib.h>
 /**
- * print_name - Prints a name using a given function
- * @name: The name to be printed
- * @f: Pointer to the function used to print the name
- *
- * Description: This function takes a name and 
- * a function pointer as arguments.
- * It applies the function passed 
- * in the argument `f` to the name `name`.
+ * print_name - Prints a name
+ * @name: The name to print
+ * @f: The function pointer
+ * Return: Nothing
  */
 
 void print_name(char *name, void (*f)(char *))
