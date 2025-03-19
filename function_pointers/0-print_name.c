2a822ea6857813fe3c8abd379a571c96fdb60f2d
voicedhealer <vivien.bernardot@gmail.com>
2025-03-19 11:28:49 +0100

    amélioration de la fonction print_name pour clarifier les commentaires et gérer les pointeurs nuls

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,20 +1,17 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 
 /**
- * print_name - Prints a name
- * @name: The name to print
- * @f: The function pointer
- * Return: Nothing
+ * print_name - Prints a name using a given function
+ * @name: The name to be printed
+ * @f: Pointer to the function used to print the name
+ *
+ * Description: This function takes a name and a function pointer as arguments.
+ * It applies the function passed in the argument `f` to the name `name`.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
-		return;
-
-	if (name == NULL)
-		return;
-
-	f(name);
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
