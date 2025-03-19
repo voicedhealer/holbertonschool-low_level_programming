a239a1305a0c4dcebf8496d87145cde2a52477ab
voicedhealer <vivien.bernardot@gmail.com>
2025-03-19 14:42:06 +0100

    suppression de la documentation de la structure op dans function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,13 +2,6 @@
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
 
-/**
- * struct op - Structure représentant une opération
- * @op: L'opérateur sous forme de chaîne
- * @f: Pointeur vers la fonction correspondante
- *
- * Description: Associe un opérateur à une fonction d'opération
- */
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
