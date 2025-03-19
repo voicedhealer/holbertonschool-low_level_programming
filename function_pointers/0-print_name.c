63283007ed352bbd99eb31ff336ca1412479aa28
voicedhealer <vivien.bernardot@gmail.com>
2025-03-19 14:32:44 +0100

    ajout d'un espace et d'un commentaire pour améliorer la clarté de la fonction print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
+
 /**
  * print_name - Prints a name
  * @name: The name to print
  * @f: The function pointer
+ *
  * Return: Nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
