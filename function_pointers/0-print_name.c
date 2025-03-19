6be1866f7b821a9150936c11594661a5ac44c68f
voicedhealer <vivien.bernardot@gmail.com>
2025-03-19 11:49:10 +0100

    amélioration des commentaires dans la fonction print_name pour une meilleure lisibilité

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,8 +5,10 @@
  * @name: The name to be printed
  * @f: Pointer to the function used to print the name
  *
- * Description: This function takes a name and a function pointer as arguments.
- * It applies the function passed in the argument `f` to the name `name`.
+ * Description: This function takes a name and 
+ * a function pointer as arguments.
+ * It applies the function passed 
+ * in the argument `f` to the name `name`.
  */
 void print_name(char *name, void (*f)(char *))
 {
