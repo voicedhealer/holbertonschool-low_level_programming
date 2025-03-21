bfc475d88a6716659420b130734f0136487a9ba7
voicedhealer <vivien.bernardot@gmail.com>
2025-03-21 11:21:44 +0100

    amélioration de la documentation de la fonction sum_them_all et initialisation de la variable sum

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,24 +3,26 @@
 
 /**
  * sum_them_all - Return the sum of all parameters
- * @n: The number of parameters
- *
+ * @n: The numbers to add
  * Return: The sum of all parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	unsigned int i, sum;
+
+	va_start(ap, n);
+
 
 	if (n == 0)
 		return (0);
 
-	va_start(ap, n);
-
+	sum = 0;
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
 	va_end(ap);
-
 	return (sum);
+
 }
