2380c75401216cb67bb49743f0430706adbfb649
voicedhealer <vivien.bernardot@gmail.com>
2025-03-21 14:50:27 +0100

    ajout d'indentation pour améliorer la lisibilité dans le fichier 0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,20 +6,20 @@
  * @n: The number of parameters passed to the function.
  * @...: A avariable number of parameters to calculate the sum of.
  *
- * Return: If n == 0 - 0.
- *        Otherwise - the sum of all parameters.
+ * Return:  If n == 0 - 0.
+ *          Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list nums;
-unsigned int index, sum = 0;
+    va_list nums;
+        unsigned int index, sum = 0;
 
-va_start(nums, n);
+    va_start(nums, n);
 
-for (index = 0; index < n; index++)
-sum += va_arg(nums, int);
+    for (index = 0; index < n; index++)
+        sum += va_arg(nums, int);
 
-va_end(nums);
+    va_end(nums);
 
-return (sum);
+    return (sum);
 }
