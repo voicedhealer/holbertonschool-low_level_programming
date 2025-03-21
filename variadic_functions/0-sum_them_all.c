a1db519792f26fdbd937bac7a864b230839fb275
voicedhealer <vivien.bernardot@gmail.com>
2025-03-21 09:39:46 +0100

    ajout de la fonction sum_them_all pour calculer la somme de paramètres variadiques

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Calcule la somme de tous ses paramètres
+ * @n: Nombre de paramètres passés à la fonction
+ * @...: Paramètres variables à additionner
+ *
+ * Return: Somme des paramètres, 0 si n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+}
