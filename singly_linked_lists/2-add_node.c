e297dfc966a588e1107c21020bf0d6bea9a7c4e8
voicedhealer <vivien.bernardot@gmail.com>
2025-04-02 11:19:15 +0200

    amélioration de la fonction add_node : mise à jour des commentaires et simplification du code

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,35 +1,40 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * add_node - Ajoute un nouveau nœud au début d'une liste chaînée
- * @head: Double pointeur vers la tête de la liste
- * @str: Chaîne de caractères à stocker dans le nouveau nœud
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to be duplicated and stored in the new node
  *
- * Return: Adresse du nouveau nœud, ou NULL en cas d'échec
+ * Return: Address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+	list_t *new_node;
 
-    if (!head)
-        return (NULL);
+	if (!head) /* Check if head is NULL */
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (!new_node)
-        return (NULL);
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	/* Duplicate the string and assign it to the new node */
+	new_node->str = strdup(str);
+	if (!new_node->str)
+	{
+		free(new_node); /* Free allocated memory if strdup fails */
+		return (NULL);
+	}
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+	/* Calculate the length of the string */
+	new_node->len = strlen(str);
 
-    return (new_node);
+	/* Link the new node to the current head of the list */
+	new_node->next = *head;
+
+	/* Update the head to point to the new node */
+	*head = new_node;
+
+	return (new_node);
 }
