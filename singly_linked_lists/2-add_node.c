3f1df8b3fc7a85f5ee6314f875d9b5ecd5c6e9fb
voicedhealer <vivien.bernardot@gmail.com>
2025-04-02 11:06:56 +0200

    ajout d'un fichier main pour tester la fonction add_node et affichage de la liste chaînée

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,23 +13,23 @@ list_t *add_node(list_t **head, const char *str)
 {
     list_t *new_node;
 
-    if (!head)  // Vérifie si le pointeur head est valide
+    if (!head)
         return (NULL);
 
-    new_node = malloc(sizeof(list_t));  // Alloue le nouveau nœud
+    new_node = malloc(sizeof(list_t));
     if (!new_node)
         return (NULL);
 
-    new_node->str = strdup(str);  // Duplique la chaîne str
+    new_node->str = strdup(str);
     if (!new_node->str)
     {
-        free(new_node);  // Libère le nœud si strdup échoue
+        free(new_node);
         return (NULL);
     }
 
-    new_node->len = strlen(str);        // Calcule la longueur de la chaîne
-    new_node->next = *head;             // Lie le nouveau nœud à l'ancienne tête
-    *head = new_node;                   // Met à jour la tête de liste
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
 
     return (new_node);
 }
