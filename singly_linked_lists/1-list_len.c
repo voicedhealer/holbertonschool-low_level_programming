8360ea032d273b6c0323506bd8ecd6651cdb369f
voicedhealer <vivien.bernardot@gmail.com>
2025-04-02 11:59:57 +0200

    mise à jour de la documentation de la fonction list_len et ajout de l'inclusion de stddef.h

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,11 +1,12 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stddef.h>
 
 /**
- * list length - counts the number of elemnets in linked list
- * @h: Pointer to the head of the list
+ * list_len - A function that calculate the length of a list
  *
- * Return: The numbers of nodes in the list
+ * @h: The beginning of the list
+ *
+ * Return: The number of nodes in the list
  */
 size_t list_len(const list_t *h)
 {
