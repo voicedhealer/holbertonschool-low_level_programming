8b6909cf78e5ea5804dfbd014aa204634c94e0c8
voicedhealer <vivien.bernardot@gmail.com>
2025-04-02 12:07:34 +0200

    amélioration de la documentation de la fonction print_list et ajustement du formatage du code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,25 +2,26 @@
 #include <stdio.h>
 
 /**
- * print_list - Prints elements of a linked list with specific formatting
- * @h: Pointer to the head of the list
+ * print_list - Prints all elements of a list_t list
+ * @h: Pointer to head node of the list
  *
  * Return: Number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
-    const list_t *current_node = h;
+	size_t node_count = 0;
+	const list_t *current_node = h;
 
-    while (current_node != NULL)
-    {
-        if (current_node->str)
-            printf("[%u] %s\n", current_node->len, current_node->str);
-        else
-            printf("[0] (nil)\n");
-        
-        node_count++;
-        current_node = current_node->next;
-    }
-    return (node_count);
+	while (current_node != NULL)
+	{
+		if (current_node->str)
+			printf("[%u] %s\n", current_node->len, current_node->str);
+		else
+			printf("[0] (nil)\n");
+
+		node_count++;
+		current_node = current_node->next;
+	}
+
+	return (node_count);
 }
