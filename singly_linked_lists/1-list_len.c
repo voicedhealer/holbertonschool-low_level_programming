b3112e35ec277068eb8f7ff75a45507bb39b0e0a
voicedhealer <vivien.bernardot@gmail.com>
2025-04-02 14:11:18 +0200

    ajout de l'inclusion de "lists.h" et amélioration de la fonction list_len pour une meilleure lisibilité

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
-#include "lists.h"
 #include <stddef.h>
+#include "lists.h"
 
 /**
  * list_len - A function that calculate the length of a list
@@ -8,15 +8,16 @@
  *
  * Return: The number of nodes in the list
  */
+
 size_t list_len(const list_t *h)
 {
-    int nb = 0;
-    const list_t *curr = h;
+	int count = 0;
+	const list_t *curr = h;
 
-    while (curr)
-    {
-        nb++;
-        curr = curr->next;
-    }
-    return(nb);
+	while (curr)
+	{
+		count++;
+		curr = curr->next;
+	}
+	return (count);
 }
